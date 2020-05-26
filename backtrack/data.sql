c41b1c6cc18460cc52f12cd20d254945985f1969
student <andy.burchill@gmail.com>
2020-05-26 11:01:46 +1000

    add data.sql file

---
+++ backtrack/data.sql
@@ -0,0 +1,33 @@
+DROP DATABASE IF EXISTS lab_records;
+CREATE DATABASE IF NOT EXISTS lab_records;
+USE lab_records;
+
+SELECT 'CREATING DATABASE STRUCTURE' as 'INFO';
+
+DROP TABLE IF EXISTS lab_records;
+
+/*!50503 set default_storage_engine = InnoDB */;
+/*!50503 select CONCAT('storage engine: ', @@default_storage_engine) as INFO */;
+
+CREATE TABLE lab_records (
+    id              INT             NOT NULL,
+    name            VARCHAR(20)     NOT NULL,
+    data_field_one  VARCHAR(20)     NOT NULL,
+    data_field_two  VARCHAR(20)     NOT NULL,
+    created_at      DATE            NOT NULL DEFAULT NOW(),
+    PRIMARY KEY (id)
+);
+
+INSERT INTO `lab_records` VALUES (1,'lab-record-1','Weeg0pahlo','ahjainoo1X');
+SELECT SLEEP(10);
+
+INSERT INTO `lab_records` VALUES (2,'lab-record-2','Ooyoh1Eij2','mieR7at0yo');
+SELECT SLEEP(10);
+
+INSERT INTO `lab_records` VALUES (3,'lab-record-3','ohkahM4um3','oolaiwei4U');
+SELECT SLEEP(10);
+
+INSERT INTO `lab_records` VALUES (4,'lab-record-4','mey5aeK0ai','phoo4yec5A');
+SELECT SLEEP(10);
+
+INSERT INTO `lab_records` VALUES (5,'lab-record-5','oR8beem8vu','XooMas8xom');
