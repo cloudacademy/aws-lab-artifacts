a6e5d6e98cf120417fc74a549893c4aa5cfc04c8
student <andy.burchill@gmail.com>
2020-06-05 16:00:04 +1000

    Update readme

--- rds-secrets-manager/app.py
+++ rds-secrets-manager/app.py
@@ -11,20 +11,18 @@ app.config['MYSQL_DATABASE_DB'] = 'information_schema'
 app.config['MYSQL_DATABASE_HOST'] = 'db-endpoint'
 mysql.init_app(app)
 
+
 @app.route('/')
 def db_connect():
     try:
         conn = mysql.connect()
-        return jsonify(
-            db_status="CONNECTED",
-            password=app.config['MYSQL_DATABASE_PASSWORD']
-        )
+        return jsonify(db_status="CONNECTED",
+                       password=app.config['MYSQL_DATABASE_PASSWORD'])
     except Exception as error:
-        return jsonify(
-            db_status="DISCONNECTED",
-            config=app.config['MYSQL_DATABASE_PASSWORD'],
-            error_message=str(error)
-        )
+        return jsonify(db_status="DISCONNECTED",
+                       password=app.config['MYSQL_DATABASE_PASSWORD'],
+                       error_message=str(error))
+
 
 if __name__ == '__main__':
     # Make the server publicly available by default
